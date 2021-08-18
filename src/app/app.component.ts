133085ec0d513fdaeb5d8eca5e53a850f25be8ee
Mohamed <mmsmefcis@gmail.com>
2021-08-18 22:20:35 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sweetalert-demo';
+
+  tinyAlert() {
+    Swal.fire({
+      html: '<img src="assets/succes.svg" class="icon"></img><h3>Your Account Verified</h3>',
+      showConfirmButton: false,
+    });
+  }
 }
